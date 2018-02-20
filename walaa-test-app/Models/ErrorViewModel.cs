3176edb2b06d10dc4bead24882c3ecf3afd38104
Wala’a Rawashdeh <wrawashdeh@dopravo.com>
2018-02-20 10:25:29 +0200

    Add project files.

---
+++ walaa-test-app/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace walaa_test_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
